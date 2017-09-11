493fe4f20732f1ced54cf175dc0e1220b6c410be
Nilay Patel <nilaypatel044@gmail.com>
2017-09-10 22:45:01 -0400

    checkpoint7-services-part2: completed

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,12 +1,13 @@
 (function(){
-    function AlbumCtrl(Fixtures) {
+    function AlbumCtrl(Fixtures, SongPlayer) {
         this.albumData = Fixtures.getAlbum();
+        this.songPlayer = SongPlayer;
         this.title = albumData.title;
         this.artist = albumData.artist;
     }
 
     angular
         .module('blocJams')
-        .controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
+        .controller('AlbumCtrl', ['Fixtures', 'SongPlayer', AlbumCtrl]);
 
 })();
