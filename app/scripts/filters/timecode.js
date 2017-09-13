0fbd4b49bf87f2b0ea38c06d506bcc7733160932
Nilay Patel <nilaypatel044@gmail.com>
2017-09-13 15:08:57 -0400

    Added the timecode.js file

---
+++ app/scripts/filters/timecode.js
@@ -0,0 +1,18 @@
+(function() {
+    function timecode() {
+        return function(seconds) {
+            var seconds = Number.parseFloat(seconds);
+
+            if (Number.isNaN(seconds)) {
+                return '-:--';
+            }
+
+            return buzz.toTimer(seconds);
+
+        };
+    }
+
+    angular
+        .module('blocJams')
+        .filter('timecode', timecode);
+})();
